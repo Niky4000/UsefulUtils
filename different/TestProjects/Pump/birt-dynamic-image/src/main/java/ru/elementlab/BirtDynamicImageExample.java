a7c5bdb7394194c5cc85874424dbc32c2f964320
Nikita <NAnishhenko@ibs.ru>
2020-07-30 15:03:50 +0300

    #Birt tests

--- different/TestProjects/Pump/birt-dynamic-image/src/main/java/ru/elementlab/BirtDynamicImageExample.java
+++ different/TestProjects/Pump/birt-dynamic-image/src/main/java/ru/elementlab/BirtDynamicImageExample.java
@@ -1,5 +1,7 @@
 package ru.elementlab;
 
+import java.io.File;
+import java.io.FileInputStream;
 import org.eclipse.birt.core.framework.Platform;
 import org.eclipse.birt.report.engine.api.EngineConfig;
 import org.eclipse.birt.report.engine.api.IReportEngine;
@@ -10,18 +12,21 @@ import org.eclipse.birt.report.engine.api.PDFRenderOption;
 
 public class BirtDynamicImageExample {
 
-	public static void main(String[] args) throws Exception{
-		Class.forName("oracle.jdbc.OracleDriver");
-		Platform.startup();
-		IReportEngineFactory factory = (IReportEngineFactory) Platform.createFactoryObject(
-				IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
-		IReportEngine birtEngine = factory.createReportEngine(new EngineConfig());
-		IReportRunnable design=birtEngine.openReportDesign("new_report.rptdesign");
-		IRunAndRenderTask birtTask = birtEngine.createRunAndRenderTask(design);
-		PDFRenderOption options = new PDFRenderOption();
-		options.setOutputFormat(PDFRenderOption.OUTPUT_FORMAT_PDF);
-		birtTask.setRenderOption(options);
-		birtTask.run();
-		System.out.println("create task success");
-	}
+    public static void main(String[] args) throws Exception {
+        new BirtDynamicImageExample().createReport();
+    }
+
+    public void createReport() throws Exception {
+        Class.forName("oracle.jdbc.OracleDriver");
+        Platform.startup();
+        IReportEngineFactory factory = (IReportEngineFactory) Platform.createFactoryObject(IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
+        IReportEngine birtEngine = factory.createReportEngine(new EngineConfig());
+        IReportRunnable design = birtEngine.openReportDesign(getClass().getClassLoader().getResourceAsStream("hello_world.rptdesign"));
+        IRunAndRenderTask birtTask = birtEngine.createRunAndRenderTask(design);
+        PDFRenderOption options = new PDFRenderOption();
+        options.setOutputFormat(PDFRenderOption.OUTPUT_FORMAT_PDF);
+        birtTask.setRenderOption(options);
+        birtTask.run();
+        System.out.println("create task success");
+    }
 }
