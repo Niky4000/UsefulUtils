f0b42dab6fcddfe9730456ce8f3a84965f0e455a
Nikita <nvanishhenko@element-lab.ru>
2022-12-19 18:40:09 +0400

    #Docx was updated!

--- projects/ibs/DocxTestProject/src/main/java/ru/ibs/docxtestproject/bean/RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlThirdTableBean.java
+++ projects/ibs/DocxTestProject/src/main/java/ru/ibs/docxtestproject/bean/RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlThirdTableBean.java
@@ -12,6 +12,16 @@ public class RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlTh
 	private Double notAcceptedServiceSumBeforeRepeatedCheck;
 	private Double holdedServiceSum;
 	private Double serviceSumThatShouldBeHoldedOnTheNextPeriod;
+	private Integer qtAll;
+	private Double sumAll;
+	private Integer qtAll1;
+	private Double sumAll1;
+	private Integer qtAll2;
+	private Double sumAll2;
+	private Integer qtAll3;
+	private Double sumAll3;
+	private Integer qtAll4;
+	private Double sumAll4;
 
 //fil_id
 //iotd
@@ -22,6 +32,16 @@ public class RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlTh
 //SUM_PRE
 //SUM_ALL
 //«0»
+//qt_all
+//sum_all
+//qt_all1
+//sum_all1
+//qt_all2
+//sum_all2
+//qt_all3
+//sum_all3
+//qt_all4
+//sum_all4
 	public RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlThirdTableBean() {
 	}
 
@@ -36,6 +56,16 @@ public class RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlTh
 		this.notAcceptedServiceSumBeforeRepeatedCheck = array[6] != null ? ((Number) array[6]).doubleValue() : null;
 		this.holdedServiceSum = array[7] != null ? ((Number) array[7]).doubleValue() : null;
 		this.serviceSumThatShouldBeHoldedOnTheNextPeriod = array[8] != null ? ((Number) array[8]).doubleValue() : null;
+		this.qtAll = array[9] != null ? ((Number) array[9]).intValue() : null;
+		this.sumAll = array[10] != null ? ((Number) array[10]).doubleValue() : null;
+		this.qtAll1 = array[11] != null ? ((Number) array[11]).intValue() : null;
+		this.sumAll1 = array[12] != null ? ((Number) array[12]).doubleValue() : null;
+		this.qtAll2 = array[13] != null ? ((Number) array[13]).intValue() : null;
+		this.sumAll2 = array[14] != null ? ((Number) array[14]).doubleValue() : null;
+		this.qtAll3 = array[15] != null ? ((Number) array[15]).intValue() : null;
+		this.sumAll3 = array[16] != null ? ((Number) array[16]).doubleValue() : null;
+		this.qtAll4 = array[17] != null ? ((Number) array[17]).intValue() : null;
+		this.sumAll4 = array[18] != null ? ((Number) array[18]).doubleValue() : null;
 	}
 
 	public Integer getFilId() {
@@ -116,5 +146,85 @@ public class RegisterOfConclusionsBasedOnTheResultsOfMedicalAndEconomicControlTh
 
 	public void setServiceSumThatShouldBeHoldedOnTheNextPeriod(Double serviceSumThatShouldBeHoldedOnTheNextPeriod) {
 		this.serviceSumThatShouldBeHoldedOnTheNextPeriod = serviceSumThatShouldBeHoldedOnTheNextPeriod;
+	}
+
+	public Integer getQtAll() {
+		return qtAll;
+	}
+
+	public void setQtAll(Integer qtAll) {
+		this.qtAll = qtAll;
+	}
+
+	public Double getSumAll() {
+		return sumAll;
+	}
+
+	public void setSumAll(Double sumAll) {
+		this.sumAll = sumAll;
+	}
+
+	public Integer getQtAll1() {
+		return qtAll1;
+	}
+
+	public void setQtAll1(Integer qtAll1) {
+		this.qtAll1 = qtAll1;
+	}
+
+	public Double getSumAll1() {
+		return sumAll1;
+	}
+
+	public void setSumAll1(Double sumAll1) {
+		this.sumAll1 = sumAll1;
+	}
+
+	public Integer getQtAll2() {
+		return qtAll2;
+	}
+
+	public void setQtAll2(Integer qtAll2) {
+		this.qtAll2 = qtAll2;
+	}
+
+	public Double getSumAll2() {
+		return sumAll2;
+	}
+
+	public void setSumAll2(Double sumAll2) {
+		this.sumAll2 = sumAll2;
+	}
+
+	public Integer getQtAll3() {
+		return qtAll3;
+	}
+
+	public void setQtAll3(Integer qtAll3) {
+		this.qtAll3 = qtAll3;
+	}
+
+	public Double getSumAll3() {
+		return sumAll3;
+	}
+
+	public void setSumAll3(Double sumAll3) {
+		this.sumAll3 = sumAll3;
+	}
+
+	public Integer getQtAll4() {
+		return qtAll4;
+	}
+
+	public void setQtAll4(Integer qtAll4) {
+		this.qtAll4 = qtAll4;
+	}
+
+	public Double getSumAll4() {
+		return sumAll4;
+	}
+
+	public void setSumAll4(Double sumAll4) {
+		this.sumAll4 = sumAll4;
 	}
 }
